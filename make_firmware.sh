fe71238424e5be2c14dd7029136930636260a088
Tisham Dhar <whatnickd@gmail.com>
2018-08-27 16:18:43 +1000

    Add env variables for lm32 to each script

--- make_firmware.sh
+++ make_firmware.sh
@@ -1,3 +1,9 @@
+CPU=lm32
+PLATFORM=arty
+TARGET=base
+FIRMWARE=micropython
+export CPU PLATFORM TARGET FIRMWARE
+
 cd src/litex-buildenv/
 source scripts/enter-env.sh
 make gateware
--- upload_firmware.sh
+++ upload_firmware.sh
@@ -1,4 +1,12 @@
-#Drop Gateware for lm32 core and flip-flop into USBSerial
+CPU=lm32
+PLATFORM=arty
+TARGET=base
+FIRMWARE=micropython
+export CPU PLATFORM TARGET FIRMWARE
+
+cd src/litex-buildenv/
+source scripts/enter-env.sh
+#Drop Gateware for lm32 core and Flip-Flop into USBSerial
 make firmware-load
 #May require pressing the reset button here
 make gateware-load
